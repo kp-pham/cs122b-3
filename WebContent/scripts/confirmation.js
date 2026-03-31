65dc1cd7518e6cbb0807b08d379d1ee4aca9b0d9
Kevin Tran Pham <kevintp5@uci.edu>
2026-03-31 11:46:58 -0700

    feat: retrieve sales record from sessionStorage

---
+++ WebContent/scripts/confirmation.js
@@ -0,0 +1,39 @@
+function showOrderDetails() {
+    let recordsList = $("#sales-table-body");
+
+    const resultData = sessionStorage.getItem("sales");
+
+    resultData["sales"].forEach(record => {
+        let row = `
+            <tr>
+                <td>record["saleId"]</td>
+                <td>
+                    <a href="single-movie.html?=${record['movieId']}">${record["title"]}</a>
+                </td>
+                <td>${record["quantity"]}</td>
+                <td>${record["price"]}</td>
+                <td>${record["subtotal"]}</td>
+            </tr>
+        `;
+
+        recordsList.append(row);
+    });
+
+    let salesTable = $("#sales-table");
+
+    let footer = `
+        <tfoot id="sales-table-footer">
+            <tr>
+                <td colspan="3"></td>
+                <td class="text-end text-dark fw-bold">Total:</td>
+                <td>$${resultData["total"]}</td>
+            </tr>
+        </tfoot>
+    `;
+
+    salesTable.append(footer);
+
+    sessionStorage.removeItem("sales");
+}
+
+showOrderDetails();
