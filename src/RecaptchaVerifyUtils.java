0371376532380dc0b15b687df333d7cd3ca616f0
Kevin Pham <kevintp5@uci.edu>
2026-04-13 19:00:10 -0700

    feat: send POST request to reCAPTCHA server

--- src/RecaptchaVerifyUtils.java
+++ src/RecaptchaVerifyUtils.java
@@ -10,6 +10,16 @@ public class RecaptchaVerifyUtils {
     public static final String SITE_VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify";
 
     public static void verify(String gRecaptchaResponse) throws Exception {
+        URL verifyURL = new URL(SITE_VERIFY_URL);
 
+        HttpsURLConnection conn = (HttpsURLConnection) verifyURL.openConnection();
+
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("User-Agent", "Mozilla/5.0");
+        conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        String params = "secret=" + System.getenv("SECRET_KEY") + "&response=" + gRecaptchaResponse;
+
+        conn.setDoOutput(true);
     }
 }
