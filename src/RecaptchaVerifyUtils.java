26bc9b40b7e584e41b8e256b02482024d611436a
Kevin Pham <kevintp5@uci.edu>
2026-04-13 18:56:39 -0700

    feat: create class to verify reCAPTCHA

---
+++ src/RecaptchaVerifyUtils.java
@@ -0,0 +1,15 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+
+public class RecaptchaVerifyUtils {
+    public static final String SITE_VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify";
+
+    public static void verify(String gRecaptchaResponse) throws Exception {
+
+    }
+}
