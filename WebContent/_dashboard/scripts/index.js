20818151ac02cb75e0b7059a1aa9dc9ffe587eb5
Kevin Pham <kevintp5@uci.edu>
2026-04-15 09:46:19 -0700

    feat: predefined widths for columns

--- WebContent/_dashboard/scripts/index.js
+++ WebContent/_dashboard/scripts/index.js
@@ -5,25 +5,31 @@ function handleResult(resultData) {
 
     resultData.forEach(table => {
         let tableHTML = `
-            <h3>${table["name"]}</h3>
-            <table id="${table["name"]}-table" class="table table-striped">
-                <thead>
-                <tr>
-                    <th>Attribute</th>
-                    <th>Type</th>
-                </tr>
-                </thead>
-                <tbody id="${table["name"]}-table-body">
-                    ${table["columns"].map(column => {
-                        return `
-                            <tr>
-                                <td>${column["name"]}</td>
-                                <td>${column["type"]}</td> 
-                            </tr>      
-                        `;
-                    }).join("")}
-                </tbody>
-            </table>
+            <div class="table">
+                <h3>${table["name"]}</h3>
+                <table id="${table["name"]}-table" class="table table-striped" style="table-layout: fixed; width: 100%;">
+                    <colgroup>
+                        <col style="width=50%;">
+                        <col style="width=50%;">
+                    </colgroup>
+                    <thead>
+                    <tr>
+                        <th>Attribute</th>
+                        <th>Type</th>
+                    </tr>
+                    </thead>
+                    <tbody id="${table["name"]}-table-body">
+                        ${table["columns"].map(column => {
+                            return `
+                                <tr>
+                                    <td>${column["name"]}</td>
+                                    <td>${column["type"]}</td> 
+                                </tr>      
+                            `;
+                        }).join("")}
+                    </tbody>
+                </table>
+            </div>
         `;
 
         schemas.append(tableHTML);
