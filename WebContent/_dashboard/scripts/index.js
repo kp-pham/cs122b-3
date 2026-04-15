d6b732d71ca0fd7a5f5e98eb92444fe62fc97fda
Kevin Pham <kevintp5@uci.edu>
2026-04-15 09:16:06 -0700

    feat: populate tables for displaying schemas

--- WebContent/_dashboard/scripts/index.js
+++ WebContent/_dashboard/scripts/index.js
@@ -1,5 +1,35 @@
 const baseURL = window.location.origin + '/' + window.location.pathname.split('/')[1];
 
+function handleResult(resultData) {
+    const schemas = $("#schemas");
+
+    resultData.forEach(table => {
+        let tableHTML = `
+            <h3>${table["name"]}</h3>
+            <table id="${table["name"]}-table" class="table table-striped">
+                <thead>
+                <tr>
+                    <th>Attribute</th>
+                    <th>Type</th>
+                </tr>
+                </thead>
+                <tbody id="${table["name"]}-table-body">
+                    <tr>
+                        ${table["columns"].map(column => {
+                            return `
+                                <td>${column["name"]}</td>
+                                <td>${column["type"]}</td>       
+                            `;
+                        })}
+                    </tr>
+                </tbody>
+            </table>
+        `;
+
+        schemas.append(tableHTML);
+    });
+}
+
 jQuery.ajax({
     dataType: "json",
     method: "GET",
