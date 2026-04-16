a8351cd89fddc7d229653cc02e34c6409c203010
Kevin Pham <kevintp5@uci.edu>
2026-04-15 21:29:47 -0700

    feat: stored procedure to get next star id

--- WebContent/stored-procedure.sql
+++ WebContent/stored-procedure.sql
@@ -22,6 +22,20 @@ BEGIN
 --     END IF;
 END
 
+CREATE PROCEDURE get_next_star_id(OUT star_id VARCHAR(10))
+BEGIN
+    DECLARE id VARCHAR(10);
+    DECLARE prefix VARCHAR(10);
+    DECLARE number INTEGER;
+
+    SELECT MAX(id) INTO id FROM stars;
+
+    SET prefix = REGEXP_REPLACE(id, [0-9], "");
+    SET number = REGEXP_REPLACE(id, [a-zA-z], "");
+
+    SET star_id = CONCAT(prefix, number + 1);
+END;
+
 $$
 
 DELIMITER ;
