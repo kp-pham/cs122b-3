dd13172b9a97803c2eaea392a336369626799451
Kevin Pham <kevintp5@uci.edu>
2026-04-15 19:50:31 -0700

    feat: dropshadow for add stars form

--- WebContent/_dashboard/scripts/stars.js
+++ WebContent/_dashboard/scripts/stars.js
@@ -1,6 +1,6 @@
 const starsForm = $("#stars-form");
 const alertSuccess  = $("#alert-success");
-const alertFailure = $("alert-failure");
+const alertFailure = $("#alert-failure");
 
 const baseURL = window.location.origin + '/' + window.location.pathname.split('/')[1];
 
