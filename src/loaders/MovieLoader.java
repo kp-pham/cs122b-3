7eb5af1b70dafd4f253c3a914f5f20d907fe9a0c
Kevin Pham <kevintp5@uci.edu>
2026-04-18 14:09:01 -0700

    feat: construct data loaders with JDBC connection
    Data loaders receive connection from main method

--- src/loaders/MovieLoader.java
+++ src/loaders/MovieLoader.java
@@ -1,10 +1,17 @@
 package loaders;
 
+import java.sql.Connection;
 import java.io.BufferedReader;
 import java.util.Set;
 import java.util.HashSet;
 
 public class MovieLoader implements DataLoader {
+    private final Connection conn;
+
+    public MovieLoader(Connection conn) {
+        this.conn = conn;
+    }
+
     public Set<String> load(String file) {
 
     }
