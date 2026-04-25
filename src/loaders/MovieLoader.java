97c42c575d1ee8c36704088f1ef05d70c4e3c917
Kevin Pham <kevintp5@uci.edu>
2026-04-25 11:20:41 -0700

    refactor: differentiate between duplicate in file and database
    Duplicates in files are not inserted when movie does not exist in
    database

--- src/loaders/MovieLoader.java
+++ src/loaders/MovieLoader.java
@@ -64,13 +64,19 @@ public class MovieLoader implements DataLoader {
                        "       CAST(year AS UNSIGNED), " +
                        "       director, " +
                        "       FLOOR(1 + RAND() * 30) + ELT(FLOOR(1 + RAND() * 3), 0.99, 0.49, 0.00) " +
-                       "FROM movies_staging AS S" +
+                       "FROM movies_staging AS S " +
                        "WHERE id IS NOT NULL AND id != '' " +
                        "AND title IS NOT NULL AND title != '' " +
-                       "AND year REGEXP '^[0-9]+$' " +
+                       "AND year IS NOT NULL AND year != '' AND year REGEXP '^[0-9]+$' " +
                        "AND director IS NOT NULL AND director != '' " +
                        "AND NOT EXISTS ( " +
                        "    SELECT 1 FROM movies AS M WHERE M.id = S.id " +
+                       ") " +
+                       "AND id IN ( " +
+                       "    SELECT id " +
+                       "    FROM movies_staging " +
+                       "    GROUP BY id " +
+                       "    HAVING COUNT(*) = 1 " +
                        ")";
 
         PreparedStatement statement = conn.prepareStatement(query);
@@ -100,14 +106,14 @@ public class MovieLoader implements DataLoader {
                        "OR title IS NULL OR title = '' " +
                        "OR year IS NULL OR year = '' OR year NOT REGEXP '^[0-9]+$' " +
                        "OR director IS NULL OR director = '' " +
+                       "OR EXISTS ( " +
+                       "    SELECT 1 FROM movies AS M WHERE M.id = S.id " +
+                       ") " +
                        "OR id IN ( " +
                        "    SELECT id" +
                        "    FROM movies_staging " +
                        "    GROUP BY id " +
                        "    HAVING COUNT(*) > 1 " +
-                       ") " +
-                       "OR EXISTS ( " +
-                       "    SELECT 1 FROM movies AS M WHERE M.id = S.id " +
                        ")";
 
         PreparedStatement statement = conn.prepareStatement(query);
@@ -126,127 +132,3 @@ public class MovieLoader implements DataLoader {
         statement.close();
     }
 }
-
-//public class MovieLoader implements DataLoader {
-//    private final Connection conn;
-//    private final Set<String> movieIds;
-//
-//    private final int EXPECTED_FIELDS_LENGTH = 4;
-//
-//    public MovieLoader(Connection conn) {
-//        this.conn = conn;
-//        this.movieIds = new HashSet<>();
-//    }
-//
-//    public Set<String> load(String file) {
-//        try (BufferedReader reader = new BufferedReader(new FileReader("movies.csv"))) {
-//            String header = reader.readLine();
-//
-//            if (header == null) {
-//                return Set.of();
-//            }
-//
-//            String query = "INSERT INTO movies VALUES (?, ?, ?, ?, ?)";
-//            PreparedStatement statement = conn.prepareStatement(query);
-//
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                String[] fields = line.split(",");
-//                process(fields, statement);
-//            }
-//
-//        } catch (Exception e) {
-//           e.printStackTrace();
-//        }
-//    }
-//
-//    private boolean validHeader(String[] fields) {
-//        return  fields.length == 4 &&
-//                fields[0].equalsIgnoreCase("id") &&
-//                fields[1].equalsIgnoreCase("title") &&
-//                fields[2].equalsIgnoreCase("year") &&
-//                fields[3].equalsIgnoreCase("director");
-//    }
-//
-//    private void process(String[] fields, PreparedStatement statement) throws SQLException {
-//        if (!validFields(fields)) {
-//            System.out.printf("Expected %d fields, Received %d fields: %s\n", EXPECTED_FIELDS_LENGTH, fields.length, String.join(", ", fields));
-//        }
-//
-//        for (int i = 0; i < fields.length; ++i) {
-//            fields[i] = fields[i].trim();
-//        }
-//
-//        String id = fields[0], title = fields[1], year = fields[2], director = fields[3];
-//
-//        if (!validId(id)) {
-//            System.out.printf("Invalid id: %s\n", String.join(", ", fields));
-//
-//        } else if (!uniqueId(id)) {
-//            System.out.printf("Duplicate movie: %s\n", String.join(", ", fields));
-//
-//        } else if (!validTitle(title)) {
-//            System.out.printf("Invalid title: %s\n", String.join(",", fields));
-//
-//        } else if (!validYear(year)) {
-//            System.out.printf("Invalid year: %s\n", String.join(",", fields));
-//
-//        } else if (!validDirector(director)) {
-//            System.out.printf("Invalid director: %s\n", String.join(",", fields));
-//
-//        } else {
-//            for (int i = 0; i < fields.length; ++i) {
-//                statement.setObject(i + 1, fields[i]);
-//            }
-//
-//            statement.setDouble(fields.length, getPrice());
-//
-//            statement.addBatch();
-//
-//            movieIds.add(id);
-//        }
-//    }
-//
-//    private boolean validFields(String[] fields) {
-//        return fields.length == EXPECTED_FIELDS_LENGTH;
-//    }
-//
-//    private boolean validId(String id) {
-//        return id != null && !id.isEmpty();
-//    }
-//
-//    private boolean uniqueId(String id) {
-//        return !movieIds.contains(id);
-//    }
-//
-//    private boolean validTitle(String title) {
-//        return title != null && !title.isEmpty();
-//    }
-//
-//    private boolean validYear(String year) {
-//        if (year == null || year.isEmpty()) {
-//            return false;
-//        }
-//
-//        try {
-//            Integer.parseInt(year);
-//            return true;
-//
-//        } catch (Exception e) {
-//            return false;
-//        }
-//    }
-//
-//    private boolean validDirector(String director) {
-//        return director != null && !director.isEmpty();
-//    }
-//
-//    private double getPrice() {
-//        double[] values = {0.00, 0.49, 0.99};
-//
-//        int dollars = (int) (Math.random() * 30) + 1;
-//        double cents = values[(int)(Math.random() * 3)];
-//
-//        return dollars + cents;
-//    }
-//}
