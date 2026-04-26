de939ad70910c05e109ceec440cf59e0df71a381
Kevin Pham <kevintp5@uci.edu>
2026-04-25 21:22:25 -0700

    feat: load movie genre relationships into staging

--- src/loaders/GenresInMoviesLoader.java
+++ src/loaders/GenresInMoviesLoader.java
@@ -29,7 +29,18 @@ public class GenresInMoviesLoader extends DataLoader {
 
     @Override
     protected void loadToStaging(String file) throws SQLException {
+        String query = "LOAD DATA LOCAL INFILE ? " +
+                       "INTO TABLE genres_in_movies_staging " +
+                       "FIELDS TERMINATED BY ',' " +
+                       "ENCLOSED BY '\"' " +
+                       "LINES TERMINATED BY '\\r\\n' " +
+                       "IGNORE 1 ROWS";
 
+        PreparedStatement statement = conn.prepareStatement(query);
+        statement.setString(1, file);
+
+        statement.executeUpdate();
+        statement.close();
     }
 
     @Override
