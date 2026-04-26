8204d3c3a2a46c69263b4e6f1fbbe8421a1ca324
Kevin Pham <kevintp5@uci.edu>
2026-04-25 21:44:01 -0700

    feat: reprot errors when loading movie genre relationships

--- src/loaders/GenresInMoviesLoader.java
+++ src/loaders/GenresInMoviesLoader.java
@@ -74,6 +74,43 @@ public class GenresInMoviesLoader extends DataLoader {
 
     @Override
     protected void reportErrors() throws SQLException {
+        String query = "WITH dupes AS ( " +
+                       "    SELECT genreId, movieId " +
+                       "    FROM genres_in_movies AS S " +
+                       "    GROUP BY genreId, movieId " +
+                       "    HAVING COUNT(*) > 1 " +
+                       ") " +
+                       "SELECT S.genreId, S.movieId, " +
+                       "CASE " +
+                       "    WHEN S.genreId IS NULL OR S.genreId = '' OR S.genreId NOT REGEXP '^[0-9]+$' THEN 'Invalid or missing genre id' " +
+                       "    WHEN S.movieId IS NULL OR S.movieId = '' THEN 'Invalid or missing movie id' " +
+                       "    WHEN D.starId IS NOT NULL AND D.movieId IS NOT NULL THEN 'Duplicate in file' " +
+                       "    WHEN G.id IS NULL THEN 'Genre id references nonexistent star' " +
+                       "    WHEN M.id IS NULL THEN 'Movie id references nonexistent movie' " +
+                       "END AS error " +
+                       "FROM genres_in_movies_staging AS S " +
+                       "LEFT JOIN dupes AS D ON D.genreId = S.genreId " +
+                       "                    AND D.movieId = S.movieId " +
+                       "LEFT JOIN genres AS G ON G.id = S.genreId " +
+                       "LEFT JOIN movies AS M ON M.id = S.movieId " +
+                       "WHERE S.genreId IS NULL OR S.genreId = '' " +
+                       "OR S.movieId IS NULL OR S.movieId = '' " +
+                       "OR D.genreId IS NOT NULL AND D.movieId IS NOT NULL " +
+                       "OR G.id IS NULL " +
+                       "OR M.id IS NULL";
 
+        PreparedStatement statement = conn.prepareStatement(query);
+        ResultSet rs = statement.executeQuery();
+
+        while (rs.next()) {
+            System.out.printf("%s: %s, %s%n",
+                              rs.getString("error"),
+                              rs.getString("genreId"),
+                              rs.getString("movieId")
+            );
+        }
+
+        rs.close();
+        statement.close();
     }
 }
